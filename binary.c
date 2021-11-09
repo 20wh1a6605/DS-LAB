a6c85fbf0b82aab03b6d1ff4f4b1fa6c43e2adff
20wh1a6605 <93636597+20wh1a6605@users.noreply.github.com>
2021-11-09 12:53:21 +0530

    Binary

--- binary.c
+++ binary.c
@@ -1,33 +1,35 @@
 #include<stdio.h>
 int binary(int a[],int key, int low, int high){
-    int mid = (high + low)/2;
     while(high >= low){
+        int mid = (high + low)/2;
         if(a[mid] == key){
             return mid;
         }
         else if(a[mid] > key){
-            low = mid + 1;
-            return binary(a,key,low,high);
-        }
-        else{
             high = mid - 1;
             return binary(a,key,low,high);
         }
-        
+        else if(a[mid]<key){
+            low = mid + 1;
+            return binary(a,key,low,high);
+        } 
+        else if(low>high){ 
+            return -1;  
+        }
     }
-    return -1;
+    
 }
 int main(){
     int a;
-    printf("Give elements number");
-    scanf("%d\n",&a);
+    printf("Give number of elements\n");
+    scanf("%d",&a);
     int arr[100],i,j,key,pos;
-    printf("Give array elements");
+    printf("Give array elements\n");
     for(i=0;i<a;i++){
-        scanf("%d\n",&arr[i]);
+        scanf("%d",&arr[i]);
     }
     printf("key\n");
-    scanf("%d\n",&key);
+    scanf("%d",&key);
     pos = binary(arr,key,0,a-1);
     if(pos == -1){
         printf("Not there\n");
